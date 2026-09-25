060f1b28a5e3dfc15f5c8e4b8008cae7ca7191f3
mariejosecom-web <mariejose.com@gmail.com>
2026-09-25 17:00:10 +0200

    Initialize Firebase messaging in service worker
    Set up Firebase messaging service worker with initialization and background message handling.

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,1 +1,23 @@
-BAj0tfDu2K645favJn2epR7wOgZpBalR3JEW24U0BXHhkBL4ouL8Vgr_L18dfhjcRnh8PYYp-usbYQ1B0X4dAOA
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCmbImmOuNb4k0iuVSkFeWiZ-8je8zbxmA",
+  authDomain: "forum-discussion-f47cc.firebaseapp.com",
+  databaseURL: "https://forum-discussion-f47cc-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "forum-discussion-f47cc",
+  storageBucket: "forum-discussion-f47cc.firebasestorage.app",
+  messagingSenderId: "479671843496",
+  appId: "1:479671843496:web:0b338af1d5cced5f9ce09d"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: payload.notification.icon || '/icon.png'
+  };
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
